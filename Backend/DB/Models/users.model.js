b48055ffee5f049a0d4a089c85aa61c4aa538613
Sultan2403 <sultanremilekun@gmail.com>
2026-02-07 00:44:07 +0100

    Finished up db schema

---
+++ Backend/DB/Models/users.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      select: false,
+    },
+    phoneNumber: {
+      type: String,
+      required: false,
+      trim: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+    strict: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema, "users");
+
+module.exports = User;
